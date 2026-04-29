0b9d2fce4046510249eb28b9e5245596a4393abe
rodinaayman <99316120+rodinaayman@users.noreply.github.com>
2026-04-29 04:42:34 +0300

    updateee

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const dynamic = "force-dynamic"; 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 const extractIdFromToken = (token: string) => {
   try {
